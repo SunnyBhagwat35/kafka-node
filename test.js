51cdae74d3b4efc5c9d6f5b5f513cbb48fd2430b
Sunny Bhagwat <sunnybhagwat35@gmail.com>
2024-01-11 13:29:32 +0530

    initial commit

---
+++ test.js
@@ -0,0 +1,31 @@
+const { Kafka } =  require("kafkajs");
+
+const kafka = new Kafka({
+    clientId: "my-kafka-app",
+    brokers: ['192.168.0.110:9092'],
+});
+async function init() {
+    console.log(kafka.brokers)
+  const admin = kafka.admin();
+  console.log("Admin connecting...");
+  admin.connect();
+  console.log("Admin Connected...");
+
+  console.log("Creating Topic [rider-updates]");
+
+  await admin.createTopics({
+    topics: [
+      {
+        topic: "rider-updates",
+        numPartitions: 1,
+      },
+    ],
+  });
+
+  console.log("Topic Created Success [rider-updates]");
+
+  console.log("Disconnecting Admin..");
+  await admin.disconnect();
+}
+
+init();
